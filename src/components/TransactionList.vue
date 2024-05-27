1fec32e1b463680d3276f6d932917a90e344a5a6
Ngawang <=>
2024-05-27 17:07:03 +0200

    :zap: Finger touch

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,33 +1,44 @@
 <template>
+  <div>
     <h3>History</h3>
-      <ul id="list" class="list">
-        <li v-for="(transaction, index) in transactions" 
-          :key="index" 
-          :class="transaction.amount < 0 ? 'minus' : 'plus'"
-        >
-          {{transaction.text}}
-            <span>CHF {{transaction.amount}}.-</span> 
-            <button class="delete-btn" @click="deleteTransaction(transaction.id)">
-              x
-            </button>
-        </li>
-
-      </ul>
+    <ul id="list" class="list">
+      <li
+        v-for="(transaction, index) in transactions"
+        :key="index"
+        :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        @touchstart="startTouch($event, index)"
+        @touchend="endTouch($event, index)"
+      >
+        {{ transaction.text }}
+        <span>CHF {{ transaction.amount }}.-</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
+
 <script setup>
-  import { defineProps } from 'vue'
+import { ref } from 'vue';
 
-  const props = defineProps({
-    transactions: {
-      type: Array,
-      required: true,
-    },
-  });
-  
-  const emit = defineEmits(['delete-transaction']);
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
 
-  const deleteTransaction = (id) => {
-    emit('delete-transaction', id);
-  };
+const emit = defineEmits(['delete-transaction']);
+
+const touchStartX = ref(0);
+
+const startTouch = (event, index) => {
+  touchStartX.value = event.changedTouches[0].screenX;
+};
+
+const endTouch = (event, index) => {
+  const touchEndX = event.changedTouches[0].screenX;
+  if (touchEndX > touchStartX.value + 50) {
+    emit('delete-transaction', props.transactions[index].id);
+  }
+};
 </script>
